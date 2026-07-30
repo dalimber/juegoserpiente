aa5155fcab903d204e51bdf79f60e4fb3ddd195d
dalimberAndrade <dalimberpubg@gmail.com>
2026-07-29 20:29:15 -0500

    Agregar for para pintar cuadricula horizontal

--- serpiente.js
+++ serpiente.js
@@ -30,6 +30,7 @@
     const TAMANIO_CELDA=25;
     function dibujarTablero() 
     {
+      //Pintamos las lineas verticales de la cuadricula
       for (let index = 0; index < canvas.width; index+=TAMANIO_CELDA) 
         {
           ctx.strokeStyle //coloca un color de línea, similar a fillStyle
@@ -39,6 +40,19 @@
           ctx.moveTo(xLineaVertical,yLineaVertical)//posición inicial de la figura
           yLineaVertical=canvas.height;
           ctx.lineTo(xLineaVertical,yLineaVertical)/*dibuja una línea desde la ultima posicion del graficador, en este caso lo que puso en moveTo, hasta la posicion que
+          recibe como parámetro, colocar cualquier valor*/
+          ctx.stroke()//dibuja la línea
+        }
+      //Pintamos las lineas horizontales de la cuadricula
+      for (let index = 0; index < canvas.height; index+=TAMANIO_CELDA) 
+        {
+          ctx.strokeStyle //coloca un color de línea, similar a fillStyle
+          ctx.beginPath()//se invoca siempre para iniciar un trazo
+          let xLineaHorizontal=0;
+          let yLineaHorizontal=index;
+          ctx.moveTo(xLineaHorizontal,yLineaHorizontal)//posición inicial de la figura
+          xLineaHorizontal=canvas.width;
+          ctx.lineTo(xLineaHorizontal,yLineaHorizontal)/*dibuja una línea desde la ultima posicion del graficador, en este caso lo que puso en moveTo, hasta la posicion que
           recibe como parámetro, colocar cualquier valor*/
           ctx.stroke()//dibuja la línea
         }
