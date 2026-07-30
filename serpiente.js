823aaf4d167f9da43b3b4dad82560e461cb092aa
dalimberAndrade <dalimberpubg@gmail.com>
2026-07-29 20:17:35 -0500

    Agregamos a funcion dibujar tablero pintar la cuadricula vertical

--- serpiente.js
+++ serpiente.js
@@ -7,7 +7,10 @@
     
 
     // Primera pintura del juego al cargar la página
-    dibujarTodo();
+    function iniciarJuego() {
+      dibujarTodo();
+    }
+    
 
     // =========================
     // FUNCIONES DE DIBUJO
@@ -17,8 +20,29 @@
       ctx.clearRect(0, 0, canvas.width, canvas.height);
     }
 
-    function dibujarTodo() {
+    function dibujarTodo() 
+    {
       limpiarCanvas();
+      /*dibujar cuadricula*/
+      dibujarTablero();
+    }
+
+    const TAMANIO_CELDA=25;
+    function dibujarTablero() 
+    {
+      for (let index = 0; index < canvas.width; index+=TAMANIO_CELDA) 
+        {
+          ctx.strokeStyle //coloca un color de línea, similar a fillStyle
+          ctx.beginPath()//se invoca siempre para iniciar un trazo
+          let x=index;
+          let y=0;
+          ctx.moveTo(x,y)//posición inicial de la figura
+          x=index;
+          y=canvas.height;
+          ctx.lineTo(x,y)/*dibuja una línea desde la ultima posicion del graficador, en este caso lo que puso en moveTo, hasta la posicion que
+          recibe como parámetro, colocar cualquier valor*/
+          ctx.stroke()//dibuja la línea
+        }
     }
 
 
